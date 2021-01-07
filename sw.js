4f1703f9fd82de8c490d40b053a6d3951d547199
Bleumont <fede.inch@gmail.com>
2021-01-06 21:37:59 -0300

    new post

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'v1_cache_bleumonts_blog',
+  urlsToCache = ['./', './assets/', './content', './favicon.ico'];
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
+      })
+      .catch((err) => console.log('Error', err))
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
